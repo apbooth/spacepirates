5473f6292ba34f1d47c7eac0748640dc80b02865
apbooth <166928947+apbooth@users.noreply.github.com>
2025-06-14 14:40:13 +0000

    setup initial player and enemy vars

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
+from random import randint
+
+#set up player and enemy ships vars
+
+player_captain_name = "Zolt"
+player_ship_health = 90
+player_ship_ammo = 99
+player_ship_shields = 100
+player_ship_supplies = 25
+player_ship_morale = 100
+
+first_main_enemy_name = "Space guard one"
+first_main_enemy_health = 100
+first_main_enemy_ammo = 100
+first_main_enemy_shields = 100
+first_main_enemy_content = 50
+
+second_main_enemy_name = "Space guard two"
+second_main_enemy_health = 90
+second_main_enemy_ammo = 80
+second_main_enemy_shields = 90
+second_main_enemy_content = 30
+
+third_main_enemy_name = "Space guard three"
+third_main_enemy_health = 70
+third_main_enemy_ammo = 60
+third_main_enemy_shields = 40
+third_main_enemy_content = 10
+
+first_cargo_ship_name = "Cargo ship one"
+first_cargo_ship_health = 100
+first_cargo_ship_shields = 50
+first_cargo_ship_ammo = 50
+first_cargo_ship_cargo = 100
+
+second_cargo_ship_name = "Cargo ship two"
+second_cargo_ship_health = 100
+second_cargo_ship_shields = 90
+second_cargo_ship_ammo = 45
+second_cargo_ship_cargo = 90
+
+third_cargo_ship_name = "Cargo ship three"
+third_cargo_ship_health = 100
+third_cargo_ship_shields = 80
+third_cargo_ship_ammo = 100
+third_cargo_ship_cargo = 68
+
+fourth_cargo_ship_name = "Cargo ship four"
+fourth_cargo_ship_health = 100
+fourth_cargo_ship_shields = 100
+fourth_cargo_ship_ammo = 100
+fourth_cargo_ship_cargo = 100
+
+fith_cargo_ship_name = "Cargo ship five"
+fith_cargo_ship_health = 100
+fith_cargo_ship_shields = 50
+fith_cargo_ship_ammo = 50
+fith_cargo_ship_cargo = 100
+
+print('You are captain {captain_name}\nYour ships health is {ships_health}%\nYou have {ships_ammo} rounds of ammo\nThe ships shields are at {ships_shields}%\nShips supplies are {ships_supplies}% and morale is {ships_morale}%'.format(captain_name=player_captain_name,ships_health=player_ship_health, ships_ammo=player_ship_ammo, ships_shields=player_ship_shields, ships_supplies=player_ship_supplies, ships_morale=player_ship_morale ))
 
