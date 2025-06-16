79ee5bbedab5134d58553a41d2645673cbc28a24
apbooth <166928947+apbooth@users.noreply.github.com>
2025-06-16 13:48:22 +0000

    reduced variables for enemies and player

--- main.py
+++ main.py
@@ -3,61 +3,26 @@ from random import randint
 #set up player and enemy ships vars
 
 player_captain_name = "Zolt"
-player_ship_health = 90
-player_ship_ammo = {'laser': 100,
-                    'cannon': 25 }
-player_ship_shields = 100
-player_ship_supplies = 25
-player_ship_morale = 100
+player_ship_health = 100
+player_ship_ammo = {'laser': 25,
+                    'cannon': 5 }
+player_ship_shields = False
+player_ship_loot = 25
+player_ship_morale = randint(75, 100)
 
-first_main_enemy_name = "Space guard one"
-first_main_enemy_health = 100
-first_main_enemy_ammo = 100
-first_main_enemy_shields = 100
-first_main_enemy_content = 50
 
-second_main_enemy_name = "Space guard two"
-second_main_enemy_health = 90
-second_main_enemy_ammo = 80
-second_main_enemy_shields = 90
-second_main_enemy_content = 30
+cargo_ship_health = 100
+cargo_ship_ammo = {'laser': 50,
+                    'cannon': 2}
+cargo_ship_health_shields = True
+cargo_ship_content = randint(25, 100)
 
-third_main_enemy_name = "Space guard three"
-third_main_enemy_health = 70
-third_main_enemy_ammo = 60
-third_main_enemy_shields = 40
-third_main_enemy_content = 10
 
-first_cargo_ship_name = "Cargo ship one"
-first_cargo_ship_health = 100
-first_cargo_ship_shields = 50
-first_cargo_ship_ammo = {'laser': 50,
-                         'cannon': 5}
-first_cargo_ship_cargo = 100
+force_ship_health = 100
+force_ship_ammo = {'laser': 50,
+                    'cannon': 7}
+force_ship_shields = True
 
-second_cargo_ship_name = "Cargo ship two"
-second_cargo_ship_health = 100
-second_cargo_ship_shields = 90
-second_cargo_ship_ammo = 45
-second_cargo_ship_cargo = 90
-
-third_cargo_ship_name = "Cargo ship three"
-third_cargo_ship_health = 100
-third_cargo_ship_shields = 80
-third_cargo_ship_ammo = 100
-third_cargo_ship_cargo = 68
-
-fourth_cargo_ship_name = "Cargo ship four"
-fourth_cargo_ship_health = 100
-fourth_cargo_ship_shields = 100
-fourth_cargo_ship_ammo = 100
-fourth_cargo_ship_cargo = 100
-
-fith_cargo_ship_name = "Cargo ship five"
-fith_cargo_ship_health = 100
-fith_cargo_ship_shields = 50
-fith_cargo_ship_ammo = 50
-fith_cargo_ship_cargo = 100
 
 #game areas
 game_areas = [1, 2, 3]
@@ -66,7 +31,7 @@ game_areas = [1, 2, 3]
 game_state = True
 
 def ships_status():
-    print('You are captain {captain_name}\nYour ships health is {ships_health}%\nYou have {ships_ammo} rounds of ammo\nThe ships shields are at {ships_shields}%\nShips supplies are {ships_supplies}% and morale is {ships_morale}%'.format(captain_name=player_captain_name,ships_health=player_ship_health, ships_ammo=player_ship_ammo, ships_shields=player_ship_shields, ships_supplies=player_ship_supplies, ships_morale=player_ship_morale ))
+    print('You are captain {captain_name}\nYour ships health is {ships_health}%\nYou have {ships_ammo} rounds of ammo\nThe ships shields are at {ships_shields}%\nShips supplies are {ships_supplies}% and morale is {ships_morale}%'.format(captain_name=player_captain_name,ships_health=player_ship_health, ships_ammo=player_ship_ammo, ships_shields=player_ship_shields, ships_loot=player_ship_loot, ships_morale=player_ship_morale ))
 
 
 def battle(ship, choose_area):
@@ -91,8 +56,8 @@ def reduce_ammo(ship, weapon_used, choose_area):
         print(player_ship_ammo)
     if ship == 'cargo':
         if choose_area == 1:
-            first_cargo_ship_ammo[weapon_used] -= 1
-            print(first_cargo_ship_ammo)
+            cargo_ship_ammo[weapon_used] -= 1
+            print(cargo_ship_ammo)
     
 def select_weapon(ship):
     if ship == 'pirate':
